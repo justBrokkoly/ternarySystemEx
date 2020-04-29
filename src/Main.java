f82769e42eb1d1685b8fdfb7447a841b34ec94aa
JustBrokkoly <kirinator3@gmail.com>
2020-04-29 16:28:05 +0300

    change output

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args)
     {
 
+        System.out.println(getValue("+++0-(+)-0+"));
         System.out.println(getValue("+++0-(-)-0+"));
     }
 
